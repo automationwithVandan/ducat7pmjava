500d78581a436a2ca4bf8a4d5b2ab1d27b4be026
vandan mishra <shuklapriya418@gmail.com>
2025-08-19 19:57:40 +0530

    1908

---
+++ src/main/java/oops/Main.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		
+		//Parent p = new Parent();
+		//p.illness();
+		//p.property();
+		
+		Child c= new Child();
+		
+		c.illness();
+		c.property();
+		
+	}
+
+}
---
+++ src/main/java/oops/Parent.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class Parent 
+{
+	public void property()
+	{
+		System.out.println("Father has property");
+	}
+	public void illness()
+	{
+		System.out.println("Father has illness");
+	}
+
+}
